4867c70808a464f927dc963bec7c21bb0f887d3b
Chandan Chandra Shil <chandanchandrashil22@gmail.com>
2024-03-27 22:01:30 +0600

    add categories section and editors pick section also

--- src/compoments/menu/Menu.jsx
+++ src/compoments/menu/Menu.jsx
@@ -2,88 +2,31 @@ import React from "react";
 import styles from "./menu.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import MenuPost from "../menuPosts/MenuPosts";
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 function Menu() {
   return (
     <div className={styles.container}>
+      {/* MOST POPULAR SECTION START HERE  */}
       <h2 className={styles.subtitle}>What is hot</h2>
       <h1 className={styles.title}>Most popular</h1>
-      <div className={styles.items}>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" fill alt="" className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category}  ${styles.travel}`}>
-              Travel
-            </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur.</h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>John Doe - </span>
-              <span className={styles.date}>27.03.2024</span>
-            </div>
-          </div>
-        </Link>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" fill alt="" className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category}  ${styles.culture}`}>
-              Culture
-            </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur.</h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>John Doe - </span>
-              <span className={styles.date}>27.03.2024</span>
-            </div>
-          </div>
-        </Link>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" fill alt="" className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category}  ${styles.food}`}>
-              Food
-            </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur.</h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>John Doe - </span>
-              <span className={styles.date}>27.03.2024</span>
-            </div>
-          </div>
-        </Link>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" fill alt="" className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category}  ${styles.coding}`}>
-              Coding
-            </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur.</h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>John Doe - </span>
-              <span className={styles.date}>27.03.2024</span>
-            </div>
-          </div>
-        </Link>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" fill alt="" className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category}  ${styles.style}`}>
-              Style
-            </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur.</h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>John Doe - </span>
-              <span className={styles.date}>27.03.2024</span>
-            </div>
-          </div>
-        </Link>
-      </div>
+      <MenuPosts withImage={false} />
+      {/* MOST POPULAR SECTION END  HERE  */}
+
+      {/* CATEGORIES SECTION START HERE  */}
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+
+      <MenuCategories/>
+
+      {/* CATEGORIES SECTION ENF HERE  */}
+
+      {/* EDITORS PICK SECTION START HERE  */}
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+      <MenuPosts withImage={true} />
+      {/* EDITORS PICK SECTION END HERE  */}
     </div>
   );
 }
