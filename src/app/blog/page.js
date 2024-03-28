fbda7890bf7bca974d6d07a04a2cbc89b656d4c4
Chandan Chandra Shil <chandanchandrashil22@gmail.com>
2024-03-28 23:15:24 +0600

    added blogPage

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./blogPage.module.css";
+import CardList from "@/compoments/cardList/CardList";
+import Menu from "@/compoments/menu/Menu";
 function BlogPage() {
   return (
-    <div>Blog Page</div>
-  )
+    <div className={styles.container}>
+      <h1 className={styles.title}>Style Blog</h1>
+      <div className={styles.content}>
+        <CardList />
+        <Menu/>
+      </div>
+    </div>
+  );
 }
 
-export default BlogPage
+export default BlogPage;
